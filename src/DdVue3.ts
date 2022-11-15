9fed1fffd3f54f244d90bdd5a574ea3136d39211
whj768702 <whj768702@gmail.com>
2022-11-15 11:36:17 +0800

    feat(lib): delete test component

--- src/DdVue3.ts
+++ src/DdVue3.ts
@@ -1,5 +1,4 @@
-import type { App, Component } from 'vue';
-import { Hello } from './components/index';
+import type {App} from 'vue';
 
 import List from './components/vddl-list.vue';
 import Draggable from './components/vddl-draggable.vue';
@@ -9,19 +8,17 @@ import Placeholder from './components/vddl-placeholder.vue';
 
 
 const install = (app: App, options: { msg: string }) => {
-  app.config.globalProperties.vddlDropEffectWorkaround = {dropEffect: null};
-  app.config.globalProperties.vddlDragTypeWorkaround = {
-    isDragging: false,
-    dragType: '',
-  };
-  app.component('vddl-draggable', Draggable);
-  app.component('vddl-list', List);
-  app.component('vddl-nodrag', Nodrag);
-  app.component('vddl-handle', Handle);
-  app.component('vddl-placeholder', Placeholder);
-  app.component('dd-hello', Hello as Component<{ msg: string }>);
-  app.provide('msg', options.msg);
+	app.config.globalProperties.vddlDropEffectWorkaround = {dropEffect: null};
+	app.config.globalProperties.vddlDragTypeWorkaround = {
+		isDragging: false,
+		dragType: '',
+	};
+	app.component('vddl-draggable', Draggable);
+	app.component('vddl-list', List);
+	app.component('vddl-nodrag', Nodrag);
+	app.component('vddl-handle', Handle);
+	app.component('vddl-placeholder', Placeholder);
 }
 
-export default { install };
-export { Hello, List, Draggable }
+export default {install};
+export {List, Draggable, Nodrag, Handle, Placeholder}
